24f5947b01625f020eb1e016d4a84b69303f60df
Mohanakhil <akhilmohan768@gmail.com>
2022-12-19 16:59:36 +0530

    homework problems

--- src/main/3ProblemsHW/BinarySearchTailRecursion.scala
+++ src/main/3ProblemsHW/BinarySearchTailRecursion.scala
@@ -5,19 +5,48 @@ object BinarySearchTailRecursion
   val binarySearch: (List[Int],Int)=>(Boolean,Int) = (list:List[Int], n:Int)=>{
     @tailrec
     def binarySearch(list:List[Int], left:Int, right:Int):(Boolean,Int)={
-      left<=right match {
-        case true=> if(n==list((left+right)/2)) (true,(left+right)/2)
-        else if(n>list((left+right)/2)) {
-          binarySearch(list,((left+right)/2)+1,right)}
-          else binarySearch(list,left, (left+right)/2-1)
-        case false=>(false,-1)
+      val mid:Int=(left+right)/2
+      if (left <= right)
+      {
+        if (n == list(mid)) (true, mid)
+        else if (n > list((left + right) / 2))
+        {
+          binarySearch(list, mid + 1, right)
+        }
+        else binarySearch(list, left, mid - 1)
+      }
+      else
+      {
+        (false, -1)
       }
     }
     binarySearch(list,0,list.size-1)
   }
+
+  val binary: (List[Int],Int)=> Boolean = (list:List[Int], n:Int)=>{
+    @tailrec
+    def binarySearch(list:List[Int]): Boolean ={
+      list match {
+        case Nil=> false
+        case h::Nil=>if(h==n) true
+        else false
+        case _=>
+          val mid=list(list.length/2)
+          if(mid==n) true
+          else if(n>mid){
+            binarySearch(list.slice((list.length/2)+1,list.length))
+          }
+          else binarySearch(list.slice(0, list.length/2))
+      }
+    }
+    binarySearch(list)
+  }
+
  def main(Args:Array[String]):Unit={
-   val list:List[Int]=List(3,4,5,6,7,8)
-   val n:Int=1
+   val list:List[Int]=List(3,4,5,6,7,8,9,10,11)
+   val n:Int=10
    println(binarySearch(list,n))
+
+   println(binary(list,n))
  }
 }
--- src/main/3ProblemsHW/LongestConsecutiveSequence.scala
+++ src/main/3ProblemsHW/LongestConsecutiveSequence.scala
@@ -7,7 +7,8 @@ object LongestConsecutiveSequence
     def longestConsecutiveSequence(list:List[Int], n:Int, count:Int, max:Int,last:Int):(Int,Int)={
       list match {
         case Nil=>(n,max)
-        case first::Nil=> if(first==last) {
+        case first::Nil=> if(first==last)
+        {
           if(count+1>max) longestConsecutiveSequence(list.tail,first,0,count+1,list.head)
           else longestConsecutiveSequence(list.tail,n,0,max,list.head)
         }
@@ -16,7 +17,8 @@ object LongestConsecutiveSequence
           longestConsecutiveSequence(list.tail,n,count+1,max,list.head)
         }
         else{
-          if(count>max){
+          if(count>max)
+          {
             longestConsecutiveSequence(list.tail,list.head,0,count+1,list.head)
           }
           else longestConsecutiveSequence(list.tail,n,0,max,list.head)
@@ -28,14 +30,7 @@ object LongestConsecutiveSequence
 
 
   def main(Args:Array[String]):Unit={
-    val list:List[Int]=List(1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4)
+    val list:List[Int]=List(1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4)
     println(longestSequence(list))
-    val max:Int=0
-    val req:Int=0
-    val count:Int=0
-    val last:Int=list.head
-//    list.foldLeft((max,req,count))((acc,ele)=>{
-//
-//    })
   }
 }
